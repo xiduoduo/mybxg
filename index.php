c41e343b3eabb33294d22f1c910b59279801d146
xiduoduo <626506518@qq.com>
2017-08-28 09:08:03 +0800

    修复主页菜单跳转

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$dir="main";
+$filename="index";
+//    var_dump($_SERVER);
+if(array_key_exists('PATH_INFO',$_SERVER)){
+    $path=$_SERVER['PATH_INFO'];
+    $str=substr($path,1);
+    $arr=explode("/",$str);
+    if(count($arr)==2){
+        $dir=$arr[0];
+        $filename=$arr[1];
+    }else{
+        $filename="login";
+    }
+}
+    include("./views/".$dir."/".$filename.".html");
+
+
+?>
