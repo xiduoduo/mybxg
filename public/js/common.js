496d7b2e971560f27df06be3a055cbebb107acd7
xiduoduo <626506518@qq.com>
2017-08-31 21:34:09 +0800

    模板引擎重构登录提交

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
     //NProgress.start();
     //NProgress.done();
 //控制左侧菜单的折叠和展开
@@ -27,8 +27,14 @@ define(['jquery','cookie'],function($){
 
     //获取登录信息
     var loginInfo= $.cookie('loginInfo');
-    var info=JSON.parse(loginInfo);
-    $('.profile img').attr('src',info.tc_avatar);
-    $('.profile h4').html(info.tc_name);
+    var info=loginInfo?JSON.parse(loginInfo):{};
+
+    var tplstr='<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+    var html=template.render(tplstr,info);
+    $('.aside .profile').html(html);
+
+    //写死了
+    //$('.profile img').attr('src',info.tc_avatar);
+    //$('.profile h4').html(info.tc_name);
 
 })
--- public/js/login.js
+++ public/js/login.js
@@ -9,7 +9,7 @@ define(['jquery','cookie'],function($){
             dataType:'json',
             success:function(data ){
                 if(data.code==200){
-                    $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
+                     $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
                     location.href='/main/index'
                 }else{
                     alert('用户名或密码错误');
