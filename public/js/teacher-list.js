2583ef3ddb6b937e977cc56c9005daf2aa535859
xiduoduo <626506518@qq.com>
2017-09-01 11:20:18 +0800

    讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
     $.ajax({
         type:'get',
         url:'/api/teacher',
@@ -7,6 +7,28 @@ define(['jquery','template'],function($,template){
             //解析数据渲染页面
             var html=template('teacherTpl',{list:data.result});
             $('#teacherInfo').html(html);
+            //绑定预览单击事件
+            $('.preview').click(function(){
+                //获取当前记录Id
+                var td=$(this).closest('td');
+                var tcId=td.attr('data-tcId');
+                //根据Id查询数据
+                $.ajax({
+                    type:'get',
+                    url:'/api/teacher/view',
+                    data:{tc_id:tcId},
+                    dataType:'json',
+                    success:function(data){
+                        console.log(data);
+                        //解析渲染数据
+                        var html =template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        //显示弹窗
+                        $('#teacherModal').modal();
+                    }
+                })
+            })
         }
-    })
+    });
+
 })
