170e4deabdec77250d734d5f3c3ca51d263442ec
xiduoduo <626506518@qq.com>
2017-09-02 20:37:33 +0800

    添加表单验证事件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate'],function($,template,util){
     util.setMenu('/teacher/list');
 
     var tcId=util.qs('tc_id');
@@ -23,19 +23,42 @@ define(['jquery','template','util','datepicker','language'],function($,template,
         submitForm('/api/teacher/add');
     }
     //提交表单共用方法
-    function submitForm(url){
-        $('#teacherBtn').click(function(){
-            $.ajax({
-                type:'post',
-                url:url,
-                data:$('#teacherForm').serialize(),
-                dataType:'json',
-                success:function(data){
-                    if(data.code==200){
-                        location.href="/teacher/list";
-                    }
-                }
-            })
-        })
-    }
+     function submitForm(url){
+         $('#teacherForm').validate({
+             sendForm:false,
+             valid:function(){
+                 //这里应该提交表单
+             },
+             description:{
+                 tc_name:{
+                     required:'请输入用户名',
+                     valid:'用户名可以使用'
+                 },
+                 tc_pass:{
+                     required:'请输入密码',
+                     pattern:'密码必须是6位数字',
+                     valid:'密码有效'
+                 },
+                 tc_join_date:{
+                     required:'请输入日期',
+                     valid:'日期有效'
+                 }
+             }
+         })
+     }
+    //function submitForm(url){
+    //    $('#teacherBtn').click(function(){
+    //        $.ajax({
+    //            type:'post',
+    //            url:url,
+    //            data:$('#teacherForm').serialize(),
+    //            dataType:'json',
+    //            success:function(data){
+    //                if(data.code==200){
+    //                    location.href="/teacher/list";
+    //                }
+    //            }
+    //        })
+    //    })
+    //}
 })
