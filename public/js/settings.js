0edbdeebd88d6efca1d865121eb5c606a591df74
xiduoduo <626506518@qq.com>
2017-09-03 11:32:24 +0800

    完成个人中心页面展示功能

---
+++ public/js/settings.js
@@ -0,0 +1,13 @@
+define(['jquery','template','util'],function($,template,util){
+    //设置导航菜单选中
+    util.setMenu('/main/index');
+    $.ajax({
+        type:'get',
+        url:'/api/teacher/profile',
+        dataType:'json',
+        success:function(data){
+            var html=template('settingsTpl',data.result);
+            $('#settingsInfo').html(html);
+        }
+    })
+})
