de3a7b71218717fbdbe6d3edfe77f8bb0d804c6d
xiduoduo <626506518@qq.com>
2017-09-05 11:51:16 +0800

    添加进度条和遮罩效果

---
+++ public/js/state.js
@@ -0,0 +1,14 @@
+define(['jquery'],function($){
+    $(document).ajaxStart(function(){
+        //控制遮罩显示
+        $('.overlay').show();
+    });
+
+    $(document).ajaxStop(function(){
+        //控制遮罩隐藏
+        setTimeout(function(){
+            $('.overlay').hide();
+        },500);
+
+    });
+})
