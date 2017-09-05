35b78b04666f5cdc96d92fa116b7dc3d46481b73
xiduoduo <626506518@qq.com>
2017-09-05 10:04:05 +0800

    个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','uploadify','datepicker','language','region'],function($,template,util){
+define(['jquery','template','util','ckeditor','uploadify','datepicker','language','region','validate','form'],function($,template,util,CKEDITOR){
     //设置导航菜单选中
     util.setMenu('/main/index');
     $.ajax({
@@ -27,6 +27,38 @@ define(['jquery','template','util','uploadify','datepicker','language','region']
             $('#pcd').region({
                 url:'/public/assets/jquery-region/region.json'
             });
+            //处理富文本
+            CKEDITOR.replace('ckeditor',{
+                toolbarGroups:[
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                    { name: 'links', groups: [ 'links' ] },
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] }
+                ]
+            });
+            //处理表单提交
+            $('#settingsForm').validate({
+                sendFrom:false,
+                valid:function(){
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    var p=$('#p options:selected').text();
+                    var c=$('#c options:selected').text();
+                    var d=$('#d options:selected').text();
+                    var hometown=p+'|'+c+'|'+d;
+                    $(this).ajaxSubmit({
+                        type:'post',
+                        url:'/api/teacher/modify',
+                        data:{tc_hometown:hometown},
+                        dataType:'json',
+                        success:function(data){
+                            if(data.code==200){
+                                location.reload();
+                            }
+                        }
+                    })
+                }
+            })
         }
     })
 })
